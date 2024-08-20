2476ba5f2ff9c75f066c64eb26c9e94f1aa15a67
iceknight17 <iceknight9717@gmail.com>
2024-08-20 12:47:08 -0500

    alpha version sqlite with logs creat and read feature

--- faucets/views.py
+++ faucets/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Log
+from .serializers import LogSerializer
 
-# Create your views here.
+class LogViewSet(viewsets.ModelViewSet):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
