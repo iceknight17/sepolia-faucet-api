305f8006099a8704d588ccfe2977cc45882d91d3
iceknight17 <iceknight9717@gmail.com>
2024-08-21 07:51:56 -0500

    finished

--- faucets/urls.py
+++ faucets/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import LogViewSet
 
 router = DefaultRouter()
-router.register(r'logs', LogViewSet)
+router.register(r'faucet', LogViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
